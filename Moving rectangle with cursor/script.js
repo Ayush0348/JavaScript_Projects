79fc665d91a50400a2ba472efbf7bd3ec533a473
Ayush <102289100+Ayush0348@users.noreply.github.com>
2023-10-03 12:38:56 +0530

    Add files via upload

---
+++ Moving rectangle with cursor/script.js
@@ -0,0 +1,15 @@
+window.addEventListener("mousemove", function(e) {
+    var rect = document.querySelector(".rect");
+
+    var xval = gsap.utils.mapRange(
+        0,
+        window.innerWidth,
+        100+rect.getBoundingClientRect().width/2,
+        window.innerWidth - (100 + rect.getBoundingClientRect().width/2),
+        e.clientX
+        );
+    gsap.to(".rect",{
+        left: xval+"px", 
+        ease: Power3
+    })
+})
